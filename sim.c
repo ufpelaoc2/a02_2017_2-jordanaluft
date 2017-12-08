7de096358d7a6f581ddd617e7825d7f641a1bf35
jordana <jordana.sluft@gmail.com>
2017-12-07 22:21:21 -0200

    removida função inutil

--- sim.c
+++ sim.c
@@ -148,17 +148,6 @@ struct stats *create_stats(int num_configs) {
   return stats;
 }
 
-
-
-
-int address_to_index(int address, struct cache config) {
-  int index, aux;
-  aux = address/config.block; // endereco / bytes por bloco
-  index = aux%(config.num_blocks);
-
-  return index;
-}
-
 int find_index_size(struct cache config) {
   return (log2(config.num_blocks));
 }
